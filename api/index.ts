08906d538c423715feaddb2368390c6bc34e19e2
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2023-09-03 17:46:55 +0530

    deployment attempt : 1

--- api/index.ts
+++ api/index.ts
@@ -1,29 +1,3 @@
-import express from "express";
-import chalk from "chalk";
-import cookieParser from "cookie-parser";
-import {
-  homeRoutes,
-  signinRoutes,
-  signupRoutes,
-  userRoutes,
-} from "../src/Routes";
-import dbConnection from "../src/utils/database";
-import "dotenv/config";
-import validateToken from "../src/middleware/validateToken";
+import app from "../app";
 
-const app = express();
-const PORT = 8080 || process.env.PORT;
-app.use(express.json());
-app.use(cookieParser());
-dbConnection();
-
-app.use("/api/v1", homeRoutes);
-app.use("/api/v1/auth/signup", signupRoutes);
-app.use("/api/v1/auth/signin", signinRoutes);
-app.use("/api/v1/users", validateToken, userRoutes);
-
-app.listen(PORT, () => {
-  console.log(
-    `server started ${chalk.yellow("http://localhost:" + PORT + "/api/v1")}`
-  );
-});
+export default app;
