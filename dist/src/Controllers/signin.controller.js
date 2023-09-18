13b823c96e17ea05cae99df7e56605d8e3913cda
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2023-09-18 10:26:19 +0530

    make secure true

--- dist/src/Controllers/signin.controller.js
+++ dist/src/Controllers/signin.controller.js
@@ -35,7 +35,7 @@ export const signinUser = (req, res) => __awaiter(void 0, void 0, void 0, functi
     res.cookie("jwt", token, {
         expires: new Date(Date.now() + 1000 * 60 * 60 * 2),
         sameSite: "none",
-        secure: false,
+        secure: true,
     });
     const sendResponseObject = {
         id: findUser === null || findUser === void 0 ? void 0 : findUser.id,
