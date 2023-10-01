533574f1eec10217efdfcc5c414f8fb2d4b0cf5e
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2023-10-01 19:25:34 +0530

    test

--- dist/src/middleware/validateToken.js
+++ dist/src/middleware/validateToken.js
@@ -1,21 +1,20 @@
 import jwt from "jsonwebtoken";
 const validateToken = (req, res, next) => {
-    var _a;
     //geting token from cookie storage, this will run if both token and cookie expire same time
     const token = req.cookies.jwt;
-    const authToken = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(" ")[1];
-    if (!token && !authToken) {
+    // const authToken: string | undefined =
+    //   req.headers.authorization?.split(" ")[1];
+    // if (!token && !authToken) {
+    //   res.status(401).json({ message: "Session Expire, Please login" });
+    //   return;
+    // }
+    if (!token) {
         res.status(401).json({ message: "Session Expire, Please login" });
         return;
     }
     //verify jwt token by expiry, only run if logout by admin, because cookie and token expiry time are same ::: eg. cookie stored in cookie storage but token is expired.
     try {
-        if (!authToken || token) {
-            jwt.verify(token, process.env.JWT_SECRET_KEY);
-        }
-        else {
-            jwt.verify(authToken, process.env.JWT_SECRET_KEY);
-        }
+        jwt.verify(token, process.env.JWT_SECRET_KEY);
         next();
     }
     catch (error) {
--- src/middleware/validateToken.ts
+++ src/middleware/validateToken.ts
@@ -4,20 +4,20 @@ import jwt from "jsonwebtoken";
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
   //geting token from cookie storage, this will run if both token and cookie expire same time
   const token = req.cookies.jwt;
-  const authToken: string | undefined =
-    req.headers.authorization?.split(" ")[1];
+  // const authToken: string | undefined =
+  //   req.headers.authorization?.split(" ")[1];
 
-  if (!token && !authToken) {
+  // if (!token && !authToken) {
+  //   res.status(401).json({ message: "Session Expire, Please login" });
+  //   return;
+  // }
+  if (!token) {
     res.status(401).json({ message: "Session Expire, Please login" });
     return;
   }
   //verify jwt token by expiry, only run if logout by admin, because cookie and token expiry time are same ::: eg. cookie stored in cookie storage but token is expired.
   try {
-    if (!authToken || token) {
-      jwt.verify(token, process.env.JWT_SECRET_KEY);
-    } else {
-      jwt.verify(authToken, process.env.JWT_SECRET_KEY);
-    }
+    jwt.verify(token, process.env.JWT_SECRET_KEY);
     next();
   } catch (error) {
     res.json({ message: "You have been logged out, Please login Again" });
