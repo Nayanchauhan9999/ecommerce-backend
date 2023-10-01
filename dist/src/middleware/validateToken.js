c5dedb98612085c1b10e8edbd503d08809155b6e
nayanchauhan9999 <nayanchauhan9999@gmail.com>
2023-10-01 19:49:07 +0530

    push old commit

--- dist/src/middleware/validateToken.js
+++ dist/src/middleware/validateToken.js
@@ -2,12 +2,6 @@ import jwt from "jsonwebtoken";
 const validateToken = (req, res, next) => {
     //geting token from cookie storage, this will run if both token and cookie expire same time
     const token = req.cookies.jwt;
-    // const authToken: string | undefined =
-    //   req.headers.authorization?.split(" ")[1];
-    // if (!token && !authToken) {
-    //   res.status(401).json({ message: "Session Expire, Please login" });
-    //   return;
-    // }
     if (!token) {
         res.status(401).json({ message: "Session Expire, Please login" });
         return;
